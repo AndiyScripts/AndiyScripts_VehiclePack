2e9b332c67da48b8d1aef953f39a8dbd1377b8cb
AndiyScripts <126016466+AndiyScripts@users.noreply.github.com>
2023-03-14 14:10:25 +0100

    Engine Sound Fix

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -12,7 +12,7 @@ files {
     'audioconfig/*_game.dat151.rel',
     'audioconfig/*_sounds.dat54.nametable',
     'audioconfig/*_sounds.dat54.rel',
-    'sfx/**/*.awc',
+    'sfx/**/*.awc'
 }
 
 data_file 'HANDLING_FILE' 'data/**/handling.meta'
@@ -26,6 +26,145 @@ data_file 'CARCONTENTUNLOCKS_FILE' 'data/**/carcontentunlocks.meta'
 data_file 'CONTENT_UNLOCKING_META_FILE' 'data/**/unlocks.meta'
 
 -- Test 1
-data_file 'AUDIO_GAMEDATA' 'audioconfig/*_game.dat'
+--[[data_file 'AUDIO_GAMEDATA' 'audioconfig/*_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audioconfig/*_sounds.dat'
-data_file 'AUDIO_WAVEPACK' 'sfx/*.awc'
+data_file 'AUDIO_WAVEPACK' 'sfx/*'
+
+-- 
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_'
+
+]]--
+
+-- 488 Sound
+data_file 'AUDIO_GAMEDATA' 'audioconfig/488sound_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/488sound_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_488sound'
+
+-- apollos v8
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/apollosv8_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/apollosv8_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_apollosv8'
+
+-- aston59 v12
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/aston59v12_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/aston59v12_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_aston59v12'
+
+-- avesv v12
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/avesvv12_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/avesvv12_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_avesvv12'
+
+-- c6 v8 sound
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/c6v8sound_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/c6v8sound_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_c6v8sound'
+
+-- diablo v12
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/diablov12_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/diablov12_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_diablov12'
+
+-- f40 v8
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/f40v8_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/f40v8_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_f40v8'
+
+-- f50 v12
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/f50v12_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/f50v12_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_f50v12'
+
+-- f136
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/f136_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/f136_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_f136'
+
+-- ferrari f12
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/ferrarif12_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/ferrarif12_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_ferrarif12'
+
+-- gta spano v10
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/gtaspanov10_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/gtaspanov10_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_gtaspanov10'
+
+-- italianna v10
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/italiannav10_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/italiannav10_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_italiannav10'
+
+-- italiantt v10
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/italianttv10_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/italianttv10_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_italianttv10'
+
+-- laferrari sound
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/laferrarisound_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/laferrarisound_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_laferrarisound'
+
+-- mclaren v8
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/mclarenv8_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/mclarenv8_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_mclarenv8'
+
+-- murcie v12
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/murciev12_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/murciev12_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_murciev12'
+
+-- mus v8
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/musv8_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/musv8_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_musv8'
+
+-- perfo v10
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/perfov10_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/perfov10_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_perfov10'
+
+-- sesto v10
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/sestov10_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/sestov10_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_sestov10'
+
+-- urus v8
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/urusv8_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/urusv8_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_urusv8'
+
+-- veyron sound
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/veyronsound_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/veyronsound_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_veyronsound'
+
+-- viper v10
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/viperv10_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/viperv10_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_viperv10'
